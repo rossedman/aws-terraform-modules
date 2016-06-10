a20d71c0edbbb41325207fc01d7f77bea67002cf
Ross Edman <ross.edman@gmail.com>
2016-06-09 20:58:09 -0500

    Add Security Group modules

---
+++ network/security/sg_http/main.tf
@@ -0,0 +1,50 @@
+/*--------------------------------------------------
+ * Variables
+ *-------------------------------------------------*/
+variable "vpc_id" {}
+variable "incoming_cidr" {default = "0.0.0.0/0"}
+variable "outgoing_cidr" {default = "0.0.0.0/0"}
+variable "app_name" {default = ""}
+variable "env" {default = ""}
+
+/*--------------------------------------------------
+ * Security Group
+ *-------------------------------------------------*/
+resource "aws_security_group" "http" {
+  name = "http"
+  description = "Security group for HTTP/HTTPS web traffic"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${split(",", var.incoming_cidr)}"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${split(",", var.incoming_cidr)}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["${split(",", var.outgoing_cidr)}"]
+  }
+
+  tags {
+    app = "${var.app_name}"
+    env = "${var.environment}"
+  }
+}
+
+/*--------------------------------------------------
+ * Output
+ *-------------------------------------------------*/
+output "id" {
+  value = "${aws_security_group.http.id}"
+}
---
+++ network/security/sg_redis/main.tf
@@ -0,0 +1,44 @@
+/*--------------------------------------------------
+ * Variables
+ *-------------------------------------------------*/
+variable "vpc_id" {}
+variable "port" {default = 6379}
+variable "incoming_cidr" {default = "0.0.0.0/0"}
+variable "outgoing_cidr" {default = "0.0.0.0/0"}
+variable "app_name" {default = ""}
+variable "env" {default = ""}
+
+/*--------------------------------------------------
+ * Security Group
+ *-------------------------------------------------*/
+resource "aws_security_group" "redis" {
+  name = "redis"
+  description = "Security group for Redis traffic"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port = "${var.port}"
+    to_port = "${var.port}"
+    protocol = "tcp"
+    cidr_blocks = ["${split(",", var.incoming_cidr)}"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["${split(",", var.outgoing_cidr)}"]
+  }
+
+  tags {
+    app = "${var.app_name}"
+    env = "${var.environment}"
+  }
+}
+
+/*--------------------------------------------------
+ * Output
+ *-------------------------------------------------*/
+output "id" {
+  value = "${aws_security_group.redis.id}"
+}
